d23f4efd43f7b351f4c514009a7cf5e9c6b6c38e
nickvaningen <nickvaningen@hotmail.com>
2017-10-13 23:45:01 +0200

    laatste update
    hoop dat dit goed is

---
+++ Drugsrunners/Assets/scripts/BackgroundLoop.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundLoop : MonoBehaviour {
+
+    [SerializeField]
+    private Image image1;
+    [SerializeField]
+    private Image image2;
+    [SerializeField]
+    private float speed;
+
+    private float idk;
+
+    // Use this for initialization
+    void Start()
+    {
+        idk = 0;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        idk += 0.01f;   
+        MoveBackground();
+    }
+
+    void MoveBackground()
+    {
+        image1.rectTransform.anchorMin = new Vector2(idk % 1, 0);
+        image1.rectTransform.anchorMax = new Vector2(idk % 1 + 1, 1);
+        image2.rectTransform.anchorMin = new Vector2(idk % 1 - 1, 0);
+        image2.rectTransform.anchorMax = new Vector2(idk % 1, 1);
+    }
+}
---
+++ Drugsrunners/Assets/scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    Text scoreTextBox;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreTextBox.text = "Score: " + Mathf.Floor(player.transform.position.x).ToString();
+	}
+}
